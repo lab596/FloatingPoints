911e36d145d357ed183a0645d4614cdc8df5818d
lab596 <86579136+lab596@users.noreply.github.com>
2022-05-29 11:02:37 -0700

    Create fractions.java

---
+++ fractions.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    //basic fraction addition
+    double fraction1 = 5/2;
+    double fraction2 = 7/2;
+    System.out.println(fraction1+fraction2); //prints: 5.0
+    //subtraction
+    System.out.println(fraction1-fraction2); //prints: 1.0
+    //multiplication
+    System.out.println(fraction1*fraction2); //prints: 6.0
+    //division
+    System.out.println(fraction1/fraction2); //prints: 0.6666666666666666
+  }
+}
