4a587d6c72b9902be63c4f50b617e6079946d36e
lab596 <86579136+lab596@users.noreply.github.com>
2022-05-29 11:03:45 -0700

    Create fractions.py

---
+++ fractions.py
@@ -0,0 +1,13 @@
+#basic fraction addition
+fraction1 = 5/2
+fraction2 = 7/2
+print(fraction1+fraction2) #prints: 6.0
+
+#fraction subtraction
+print(fraction1-fraction2) #prints: -1.0
+
+#fraction multiplication
+print(fraction1*fraction2) #prints: 8.75
+
+#fraction division
+print(fraction1/fraction2) #prints: 0.7142857142857143
