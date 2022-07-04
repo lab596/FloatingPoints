15b020a16176ee0813ff3905d81094bbdcd74e66
Rohan Udupa <86579136+lab596@users.noreply.github.com>
2022-07-04 11:44:06 -0700

    Update Webdriver.py

--- Webdriver.py
+++ Webdriver.py
@@ -1,15 +1,30 @@
-import webbrowser
-import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
+
+from selenium.webdriver.chrome.service import Service
 
 
 
-path="C:\chromedriver_win32 (1)\chromedriver.exe"
-driver = webdriver.Chrome(path)
+    
+path=Service("C:\chromedriver_win32 (1)\chromedriver.exe")
+driver = webdriver.Chrome(service=path)
 
-driver.get('http://www.32x8.com/var6.html')
+driver.get('http://www.32x8.com/var5.html')
 
+arr3 = [0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0]
+
+count = 0
+num = 3
+
+
+for i in range(32):
+    row = driver.find_element(By.XPATH,"/html/body/form/table/tbody/tr["+str(num)+"]/td[8]/input")
+    if arr3[i] == 1:
+        row.click()
+    num+=1
+    count+=1
+
+sub = driver.find_element(By.XPATH,"/html/body/form/table/tbody/tr[35]/td/input")
+sub.click()
 
